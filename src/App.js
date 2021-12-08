72ade8a6d1bb177a801aaf24642cb91c336664ca
Vraj Patel <patel.vraj3@gmail.com>
2021-12-07 22:49:51 -0500

    importing the row component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import './App.css';
+import Row from "./Row"
 
 function App() {
   return (
     <div className="App">
       <h1>This is a netflix clone</h1>
+      <Row title="" />
+      <Row title="" />
     </div>
   );
 }
